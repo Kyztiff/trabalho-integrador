d31683d3014ecb88b444b795f8e008a37bb14d46
Tiffany <tiff4nyb@gmail.com>
2026-06-07 22:06:13 -0300

    orgFinal

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,9 +2,7 @@ const jwt = require('jsonwebtoken');
 const usuarioModel = require('../models/usuarioModel');
 
 function login(req, res) {
-
     const { email, senha } = req.body;
-
     const usuario = usuarioModel.buscarPorEmail(email);
 
     if (!usuario || usuario.senha !== senha) {
@@ -12,41 +10,27 @@ function login(req, res) {
             erro: 'Credenciais inválidas'
         });
     }
-
     const token = jwt.sign(
-        {
-            id: usuario.id,
-            role: usuario.tipo
-        },
+        {id: usuario.id,
+            role: usuario.tipo},
         'segredo-super-secreto',
-        {
-            expiresIn: '15m'
+        { expiresIn: '15m'
         }
     );
-
     res.status(200).json({ token });
 }
 
 function registrar(req, res) {
-
     try {
-
         const novoUsuario = usuarioModel.criar(req.body);
-
         res.status(201)
             .set('Location', '/api/usuario/' + novoUsuario.id)
             .json(novoUsuario);
 
     } catch (err) {
-
         res.status(400).json({
             erro: err.message
         });
-
     }
 }
-
-module.exports = {
-    login,
-    registrar
-};
+module.exports = {login, registrar};
